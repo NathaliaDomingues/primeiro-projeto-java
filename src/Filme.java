02458db1d379412c4754898aec3f975017c8b63e
NathaliaDomingues <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-11 17:05:47 -0300

    Add files via upload

---
+++ src/Filme.java
@@ -0,0 +1,18 @@
+public class Filme {
+    public static void main(String[] args) {
+        System.out.println("Filme: Infiltrado na Klan");
+
+        int anoDeLancamento = 2018;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 7.5;
+
+        double media = (9.8 + 10 + 9.9) / 3;
+        System.out.println(media);
+        System.out.println("Sinopse: Em 1978, Ron Stallworth, um policial negro do Colorado, consegue se infiltrar na Ku Klux Klan local. \n" +
+                "Ele se comunica com os outros membros do grupo por meio de telefonemas e cartas, e quando precisa estar fisicamente presente, \n" +
+                "ele envia um outro policial branco em seu lugar. Depois de meses de investigação, Ron fica próximo do líder da seita, \n" +
+                "sendo responsável por sabotar uma série de linchamentos e outros crimes de ódio orquestrados pelos racistas.");
+    }
+
+}
---
+++ src/OperacoesMatematicas.java
@@ -0,0 +1,11 @@
+public class OperacoesMatematicas {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Classes.Soma:" + (a + b));
+        System.out.println("Multiplicaçâo:" + (a * b));
+        System.out.println("Subtração:" + (a - b));
+        System.out.println("Divisão:" + (a / b));
+    }
+}
