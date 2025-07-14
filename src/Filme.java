ee4cd86bc9d7e7fad3b02a35e5272f0aa36cb265
NathaliaDomingues <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-14 15:42:49 -0300

    Add files via upload

--- src/Filme.java
+++ src/Filme.java
@@ -6,13 +6,19 @@ public class Filme {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 7.5;
-
+        //Média calculada pelas 3 notas
         double media = (9.8 + 10 + 9.9) / 3;
         System.out.println(media);
-        System.out.println("Sinopse: Em 1978, Ron Stallworth, um policial negro do Colorado, consegue se infiltrar na Ku Klux Klan local. \n" +
-                "Ele se comunica com os outros membros do grupo por meio de telefonemas e cartas, e quando precisa estar fisicamente presente, \n" +
-                "ele envia um outro policial branco em seu lugar. Depois de meses de investigação, Ron fica próximo do líder da seita, \n" +
-                "sendo responsável por sabotar uma série de linchamentos e outros crimes de ódio orquestrados pelos racistas.");
-    }
+        String sinopse;
+        sinopse = """
+                Filme Infiltrado na Klan
+                Em 1978, Ron Stallworth, um policial negro do Colorado, que consegue se infiltrar na Ku Klux Klan local.
+                Filme ótimo!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
 
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
+    }
 }
