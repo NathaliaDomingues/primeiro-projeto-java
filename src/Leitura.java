25e4cbab1286626c468332dd0e472277fd816e6d
NathaliaDomingues <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-15 17:08:47 -0300

    Add files via upload

--- src/Leitura.java
+++ src/Leitura.java
@@ -6,5 +6,13 @@ public class Leitura {
         System.out.println("Digite seu filme favorito: ");
         String filme = leitura.nextLine();
         System.out.println(filme);
+
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("Digite uma avaliação:");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println("Então seu filme favorito é " + filme + " e foi lançado em " + anoDeLancamento + ", foi avaliado em " + avaliacao);
     }
 }
